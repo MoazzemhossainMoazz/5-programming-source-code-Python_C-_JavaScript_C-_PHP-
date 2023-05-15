c2c1a91f81b8d1155afd86275771553cc6cbf32f
Moazz <moazzemh222@gmail.com>
2023-05-15 23:53:10 +0600

    added chapter_02 and move to other files in chapter_01

---
+++ Chapter 02/Python/input_output_function.py
@@ -0,0 +1,11 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+print("your age is " +age)
+
+print("Your first name is ")
+print(first_name)
+
+print("Your last name is ")
+print(last_name)
+print(4+5)
