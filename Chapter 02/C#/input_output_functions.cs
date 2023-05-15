8f9bca28240ebaa383a41c5d74d8759c6f5e8e8d
Moazz <moazzemh222@gmail.com>
2023-05-16 00:59:49 +0600

    added chapter02 file

---
+++ Chapter 02/C#/input_output_functions.cs
@@ -0,0 +1,17 @@
+/* Online C# Compiler and Editor */
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your first name: ");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("Your first name is "+first_name);
+        
+        Console.WriteLine("Enter your last name");
+        string last_name = Console.ReadLine();
+        Console.WriteLine("Your last name is "+last_name);
+    }
+}
