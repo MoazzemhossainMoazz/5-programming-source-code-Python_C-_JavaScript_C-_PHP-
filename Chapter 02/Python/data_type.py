246e682e591e2bcafed6eed174ebbb3f90519ba1
Moazz <moazzemh222@gmail.com>
2023-05-17 23:51:41 +0600

    added chapter 03

---
+++ Chapter 02/Python/data_type.py
@@ -0,0 +1,4 @@
+#mutable
+lst = [82,3,54,5,64,6,75]
+print(lst)
+
---
+++ Chapter 03/Python/all_data_types.py
@@ -0,0 +1,11 @@
+str = 'skadklfs347^&#*(*)#'
+st = """asdkjfjasldt"""
+print(str[0])
+a = 84
+b = 23.43
+c = 4+8
+print(type(a))
+print(range)
+print(range(1,2,3,4,))
+
+fii=
